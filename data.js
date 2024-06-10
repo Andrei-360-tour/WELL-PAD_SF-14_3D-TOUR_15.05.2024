4cb48015dbd2a1905f90407e25ab844e505b86bc
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-06-10 10:26:22 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,495 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sf-14_overview",
+      "name": "SF-14_Overview",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.05109384562793018,
+        "pitch": 0.6048260865692221,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.026311987520040603,
+          "pitch": 0.7636940645651009,
+          "rotation": 0,
+          "target": "1-sf-14_overview-1"
+        },
+        {
+          "yaw": 0.4225673560781438,
+          "pitch": 0.4363830721311004,
+          "rotation": 0,
+          "target": "6-sf-14_ttransformer"
+        },
+        {
+          "yaw": 0.17458411046890632,
+          "pitch": 0.9917366309674662,
+          "rotation": 0,
+          "target": "3-sf-14_wellhead--pipeline"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sf-14_overview-1",
+      "name": "SF-14_Overview 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.23719399004465203,
+        "pitch": 0.83318045243605,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.103038327651827,
+          "pitch": 0.15485596810737334,
+          "rotation": 0,
+          "target": "0-sf-14_overview"
+        },
+        {
+          "yaw": -1.1674508419447918,
+          "pitch": 1.2725413386075033,
+          "rotation": 0,
+          "target": "2-sf-14_wellhead"
+        },
+        {
+          "yaw": -0.7678780784156984,
+          "pitch": 0.7592829623220396,
+          "rotation": 0,
+          "target": "4-sf-14_sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 0.4590610634647305,
+          "pitch": 0.3711310817161504,
+          "rotation": 0,
+          "target": "6-sf-14_ttransformer"
+        },
+        {
+          "yaw": 1.1573268673081092,
+          "pitch": 0.8512282849730042,
+          "rotation": 0,
+          "target": "7-sf-14_well-pad-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sf-14_wellhead",
+      "name": "SF-14_WellHead",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.2571160215656114,
+        "pitch": 0.19563781486820275,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5562337023222845,
+          "pitch": -0.9074460453185758,
+          "rotation": 0,
+          "target": "1-sf-14_overview-1"
+        },
+        {
+          "yaw": 0.9411503777633285,
+          "pitch": 0.1283180047185457,
+          "rotation": 0,
+          "target": "3-sf-14_wellhead--pipeline"
+        },
+        {
+          "yaw": -2.0265387645429964,
+          "pitch": 0.22723629999515538,
+          "rotation": 0,
+          "target": "4-sf-14_sucker-rod-pumping-system"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sf-14_wellhead--pipeline",
+      "name": "SF-14_WellHead & Pipeline",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.25627729038419034,
+        "pitch": 0.3264488981824698,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.28685422112623904,
+          "pitch": 0.29425538941111284,
+          "rotation": 0,
+          "target": "2-sf-14_wellhead"
+        },
+        {
+          "yaw": -3.060849546298897,
+          "pitch": 0.19015954976958582,
+          "rotation": 0,
+          "target": "8-sf-14_well-pad-2"
+        },
+        {
+          "yaw": -2.244844540780999,
+          "pitch": -0.3728426815694945,
+          "rotation": 0,
+          "target": "0-sf-14_overview"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sf-14_sucker-rod-pumping-system",
+      "name": "SF-14_Sucker rod pumping system",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.47123887231204087,
+        "pitch": -0.2002682956935118,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.287975697752616,
+          "pitch": 0.26876310552970395,
+          "rotation": 0,
+          "target": "2-sf-14_wellhead"
+        },
+        {
+          "yaw": -1.5666018145633007,
+          "pitch": 0.10100140944521918,
+          "rotation": 0,
+          "target": "5-sf-14_well-pad"
+        },
+        {
+          "yaw": 1.338123564041414,
+          "pitch": -0.8672645040025397,
+          "rotation": 0,
+          "target": "1-sf-14_overview-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sf-14_well-pad",
+      "name": "SF-14_Well Pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.8987080117070008,
+        "pitch": 0.2824593303422702,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.798034669968999,
+          "pitch": 0.2104165708388983,
+          "rotation": 0,
+          "target": "4-sf-14_sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 2.12823579425602,
+          "pitch": 0.08201287057161011,
+          "rotation": 0,
+          "target": "6-sf-14_ttransformer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sf-14_ttransformer",
+      "name": "SF-14_Ttransformer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.23267272545644602,
+        "pitch": 0.09260961650566557,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8575049899069462,
+          "pitch": -0.448084091227015,
+          "rotation": 0,
+          "target": "0-sf-14_overview"
+        },
+        {
+          "yaw": -2.56606878271373,
+          "pitch": 0.08240853054611108,
+          "rotation": 0,
+          "target": "5-sf-14_well-pad"
+        },
+        {
+          "yaw": 2.7877393679282134,
+          "pitch": 0.1210502016146684,
+          "rotation": 0,
+          "target": "7-sf-14_well-pad-1"
+        },
+        {
+          "yaw": 3.0258072818888078,
+          "pitch": -0.2856463585227882,
+          "rotation": 0,
+          "target": "1-sf-14_overview-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-sf-14_well-pad-1",
+      "name": "SF-14_Well Pad 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.3824811939213504,
+        "pitch": 0.12849584290161076,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8647357729652914,
+          "pitch": 0.193012649262144,
+          "rotation": 0,
+          "target": "6-sf-14_ttransformer"
+        },
+        {
+          "yaw": -0.609806776227666,
+          "pitch": 0.020091854056282443,
+          "rotation": 0,
+          "target": "8-sf-14_well-pad-2"
+        },
+        {
+          "yaw": 0.23492590076017805,
+          "pitch": -0.6058758488174192,
+          "rotation": 0,
+          "target": "1-sf-14_overview-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-sf-14_well-pad-2",
+      "name": "SF-14_Well Pad 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.707500778617801,
+        "pitch": -0.09758171669184179,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0175347019980387,
+          "pitch": 0.09071735882897869,
+          "rotation": 0,
+          "target": "7-sf-14_well-pad-1"
+        },
+        {
+          "yaw": 2.4014830557780664,
+          "pitch": 0.09979081883945184,
+          "rotation": 0,
+          "target": "3-sf-14_wellhead--pipeline"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WELL PAD SF-14",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
